591ef0bfcbe11299bad62aaac1f6032b6124f687
toolongdontread <kcltommy01@gmail.com>
2023-01-27 23:13:36 +0800

    add to do list

--- rntest2/App.js
+++ rntest2/App.js
@@ -1,34 +1,44 @@
 import * as React from 'react';
-import { Text, View, SafeAreaView, StyleSheet, Image } from 'react-native';
+import { Text, View, SafeAreaView, StyleSheet, Image, FlatList, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import * as Progress from 'react-native-progress';
-import BouncyCheckbox from 'react-native-bouncy-checkbox';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+function genQuote(){
+  const quote = ['a','b','c','d','e']
+  const ranInt = Math.floor(Math.random()*quote.length)
+  return quote[ranInt]
+}
 
 function HomeScreen() {
+  const newQuote = genQuote()
   return(
     <SafeAreaView style={styles.homeContainer}>
         <View style={styles.quoteContainer}>
             <Text style={styles.quote}>
-                Quote from celebrity
+                {newQuote}
             </Text>
         </View>
-        <View style={styles.expContainer}>
         <Image style={styles.image} source={require('./testingImage.png')}/>
+        <View>
             <Text style={styles.expText}>
                 Exp:
             </Text>
             <Progress.Bar 
-                progress={0.1} 
-                width={350} 
-                height={10}/>
+                progress={progressValue} 
+                width={310} 
+                height={10}
+                marginTop={10}/>
         </View>
-        <View style={styles.sumContainer}>
-            <Text style={styles.expText}>
-                Summary:
-            </Text>
+        <View>
+          <Text style={styles.expText}>
+            Summary:
+          </Text>
+          <FlatList/>
         </View>
     </SafeAreaView>
 );
@@ -45,30 +55,127 @@ function HabitScreen() {
 }
 
 function TaskScreen() {
-  return(
-    <View style={styles.container}>
-      <View style={styles.taskWrapper}>
-          <View style={styles.tasks}>
-            <BouncyCheckbox 
-              text='Task1'
-            />
-          </View>
-          <View style={styles.tasks}>
-            <BouncyCheckbox 
-              text='Task2'
-            />
-          </View>
-          <View style={styles.tasks}>
-            <BouncyCheckbox 
-              text='Task3'
-            />
-          </View>
+
+  const [todos, setTodos] = React.useState([
+    {id:1,task:'test1'},
+    {id:2,task:'test2'},
+    {id:3,task:'test3'},
+    {id:4,task:'test4'},
+  ])
+
+  React.useEffect(() => {
+    getTodosFromUserDevice()
+  }, [])
+  
+  React.useEffect(() => {
+    saveToUserDevice(todos)
+  }, [todos])
+  
+  const saveToUserDevice = async todos => {
+    try{
+      const jsonValue = JSON.stringify(value)
+      await AsyncStorage.setItem('@storage_Key', jsonValue)
+    } catch(e){
+      console.log(e)
+    }
+  }
+  
+  const getTodosFromUserDevice = async() => {
+    try{
+      const todos = await AsyncStorage.getItem('todos')
+      if(todos != null){
+        setTodos(JSON.parse(todos))
+      }
+    } catch(e){
+      console.log(e)
+    }
+  }
+
+  const ListItem = ({todo}) => {
+    return( 
+      <View style={styles.listItem}>
+        <View style={{flex:1}}>
+          <Text style={{fontSize:15, color:'#000'}}>{todo?.task}</Text>
         </View>
-    </View>
-);
+        <TouchableOpacity
+          style={styles.doneIcon}
+          onPress={()=>finishTask(todo?.id)}>
+            <Icon name='done' size={20} color={'#fff'}></Icon>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.deleteIcon}
+          onPress={()=>deleteTask(todo?.id)}>
+            <Icon name='delete' size={20} color={'#fff'}></Icon>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+  
+  const addTodo = () => {
+    if(textInput == ""){
+      Alert.alert("Invalid Task Name", "Please enter the task name")
+    }
+    else{
+      const newTodo = {
+        id:Math.random(),
+        task:textInput,
+      }
+      setTodos([...todos,newTodo])
+      setTextInput('')
+    }   
+  }
+
+  const deleteTask = todoId => {
+    const newTodos = todos.filter(item => item.id != todoId)
+    setTodos(newTodos)
+  }
+
+  const deleteAllTasks = () => {
+    setTodos([])
+  }
+
+  const finishTask = todoId =>{
+    const newTodos = todos.filter(item => item.id != todoId)
+    setTodos(newTodos)
+    Alert.alert('Exp Increase', 'EXP + 10')
+  }
+
+  const [textInput, setTextInput] = React.useState('')
+
+  return(
+    <SafeAreaView style={{flex:1}}>
+      <TouchableOpacity onPress={deleteAllTasks}>
+        <View style={styles.deleteAllIconContainer}>
+          <Icon name='delete' size={25} color='red'></Icon>
+        </View>
+      </TouchableOpacity>
+      <View style={styles.taskContainer}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{padding:20,paddingBottom:100}}
+          data={todos} 
+          renderItem={({item})=><ListItem todo={item}/>}/>
+      </View>
+      <View style={styles.addTaskContainer}>
+        <View style={styles.addTaskBar}>
+          <TextInput 
+            placeholder='Add habit'
+            value={textInput}
+            onChangeText={(text)=>setTextInput(text)}/>
+        </View>
+        <TouchableOpacity onPress={addTodo}>
+          <View style={styles.addButtonIconContainer}>
+            <Icon name='add' color={COLORS.white} size={30}/>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const Tab = createBottomTabNavigator();
+const COLORS = {primary:'#1f145c', white:'#fff'}
+var progressValue = 0
 
 export default function App() {
   return (
@@ -101,49 +208,93 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  addButtonIconContainer:{
+    height:50,
+    width:50,
+    backgroundColor:'#00f',
+    borderRadius:30,
+    elevation:40,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  addTaskBar:{
+    backgroundColor: COLORS.white,
+    elevation:40,
+    flex:1,
+    height:50,
+    marginVertical:20,
+    marginRight:20,
+    borderRadius:30,
+    paddingHorizontal:20,
+  },
+  addTaskContainer:{
+    position:'absolute',
+    bottom:0,
+    color: COLORS.white,
+    width:'100%',
+    flexDirection:'row',
+    alignItems:'center',
+    paddingHorizontal:20,
+  },
+  deleteAllIconContainer:{
+    padding:10,
+    position:'absolute',
+    right:0,
+  },
+  deleteIcon:{
+    height:25,
+    width:25,
+    backgroundColor:'red',
+    justifyContent:'center',
+    alignItems:'center',
+    marginLeft:5,
+    borderRadius:3,
+  },
+  doneIcon:{
+    height:25,
+    width:25,
+    backgroundColor:'green',
+    justifyContent:'center',
+    alignItems:'center',
+    marginLeft:5,
+    borderRadius:3,
+  },
   homeContainer:{
-      flex:1, 
-      alignItems:'center',
-      padding:10
+    flex:1, 
+    alignItems:'center',
+    padding:10,
+  },
+  listItem:{
+    padding:20,
+    backgroundColor:'#fff',
+    flexDirection:'row',
+    elevation:10,
+    borderRadius:10,
+    marginVertical:10,
   },
   quoteContainer:{
-      width:'90%',
-      backgroundColor:'#fff',
-      borderRadius:15,
-      padding:10,
-      alignItems:'center',
+    width:'90%',
+    backgroundColor:'#fff',
+    borderRadius:20,
+    padding:10,
+    alignItems:'center',
   },
   quote:{
-      fontSize:15,
-      fontWeight:'bold',
-  },
-  expContainer:{
-      position:'absolute',
-      bottom:100,
-      left:20,
+    fontSize:15,
+    fontWeight:'bold',
   },
   expText:{
-      fontSize:20
+    fontSize:20
   },
-  sumContainer:{
-      position:'absolute',
-      bottom:50,
-  },
-  container:{
-    flex:1,
-  },
-  taskWrapper:{
-    paddingTop:10,
-    paddingHorizontal:10,
-  },
-  tasks:{
-    backgroundColor:'#fff',
+  taskContainer:{
     padding:20,
-    borderRadius:20,
-    marginVertical:5
-  },
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'space-between',
+  }, 
   image:{
-    height:320,
-    width:370,
+    height:220,
+    width:270,
+    margin:20,
   },
 })
